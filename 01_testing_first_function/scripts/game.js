72da4e36ca3e1bf9567f58c59d58fa73e7bdda9c
Matt Rudge <matt.rudge@codeinstitute.net>
2021-01-26 16:22:44 +0000

    initial commit

---
+++ 01_testing_first_function/scripts/game.js
@@ -0,0 +1,9 @@
+let game = {
+    currentGame: [],
+    playerMoves: [],
+    score: 0,
+    choices: ["button1", "button2", "button3", "button4"]
+};
+
+
+module.exports = { game };
---
+++ 01_testing_first_function/scripts/tests/game.test.js
@@ -0,0 +1,28 @@
+const { game } = require("../game");
+
+
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});

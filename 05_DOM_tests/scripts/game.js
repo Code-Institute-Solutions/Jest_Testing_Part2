5199f444e3564333e6c64f09ca079a147b5e913a
Matt Rudge <matt.rudge@codeinstitute.net>
2021-01-27 16:52:09 +0000

    add part 5 and DOM attributes

---
+++ 05_DOM_tests/scripts/game.js
@@ -0,0 +1,58 @@
+let game = {
+    currentGame: [],
+    playerMoves: [],
+    score: 0,
+    turnNumber: 0,
+    choices: ["button1", "button2", "button3", "button4"]
+};
+
+function newGame() {
+    game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
+
+    for (let circle of document.getElementsByClassName("circle")) {
+        if (circle.getAttribute("data-listener") !== "true") {
+            circle.addEventListener("click", (e) => {
+                let move = e.target.getAttribute("id");
+                lightsOn(move);
+                game.playerMoves.push(move);
+                playerTurn();
+            });
+            circle.setAttribute("data-listener", "true");
+        }
+    }
+
+    showScore();
+    addTurn();
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    showTurns();
+}
+
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(function () {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add(circ + "light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove(circ + "light");
+    }, 400);
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };

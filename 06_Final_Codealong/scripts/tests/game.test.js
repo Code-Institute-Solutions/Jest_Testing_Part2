fe509afe1407029fd1773f6ccbd2bdfab45a865f
Matt Rudge <matt.rudge@codeinstitute.net>
2021-02-01 08:23:36 +0000

    update last test file

--- 06_Final_Codealong/scripts/tests/game.test.js
+++ 06_Final_Codealong/scripts/tests/game.test.js
@@ -37,6 +37,15 @@ describe("game object contains correct keys", () => {
     test("turnNumber key exists", () => {
         expect("turnNumber" in game).toBe(true);
     });
+    test("lastButton key exists", () => {
+        expect("lastButton" in game).toBe(true);
+    });
+    test("turnInProgress key exists", () => {
+        expect("turnInProgress" in game).toBe(true);
+    });
+    test("turnInProgress key value is false", () => {
+        expect("turnInProgress" in game).toBe(true);
+    });
 });
 
 describe("newGame works correctly", () => {
